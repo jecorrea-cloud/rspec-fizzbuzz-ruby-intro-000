bd52e0e1e7fa23597afbc702b97fdaf110a97cb5
Joaquin Correa <jecorrea@protonmail.com>
2021-03-19 00:55:01 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,14 +4,14 @@
 
 def fizzbuzz(int)
 
-  if int % 3 == 0
-    "Fizz"
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
 
   elsif int % 5 == 0
     "Buzz"
 
-  elsif (int % 3 == 0) && (int % 5 == 0)
-    "FizzBuzz"
+  elsif (int % 3 == 0)
+    "Fizz"
 
   else
     nil
